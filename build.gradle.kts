d28774c2a57f8dad6044f423b8ae2641e0e8744d
jimxl <tianxiaxl@gmail.com>
2021-10-04 22:26:39 +0800

    修复了错误

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     kotlin("jvm") version "1.5.31"
     jacoco
     id("org.sonarqube") version "3.3"
+    id("java-gradle-plugin")
+    `maven-publish`
 }
 
 group = "info.dreamcoder"
@@ -9,18 +11,23 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
     maven("https://plugins.gradle.org/m2/")
 }
 
 dependencies {
     implementation(gradleApi())
-    implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
     implementation("com.github.ajalt.mordant:mordant:2.0.0-beta2")
     implementation("io.github.microutils:kotlin-logging:2.0.11")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt")
+
+    implementation("info.dreamcoder:kotby:2a4b5647cd")
 
     implementation("com.adarshr:gradle-test-logger-plugin:3.0.0")
     implementation("org.junit.jupiter:junit-jupiter:5.8.0")
+
+    testImplementation("org.amshove.kluent:kluent:1.68")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
@@ -39,5 +46,14 @@ sonarqube {
         property("sonar.projectKey", "KotlinDream_devtools")
         property("sonar.organization", "kotlin-dream")
         property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
+gradlePlugin {
+    plugins {
+        create("migrationPlugin") {
+            id = "info.dreamcoder.devtools"
+            implementationClass = "gradle.plugins.DevTools"
+        }
     }
 }
--- src/main/kotlin/gradle/tasks/GuardTask.kt
+++ src/main/kotlin/gradle/tasks/GuardTask.kt
@@ -4,6 +4,8 @@ import gradle.tasks.guard.Console
 import gradle.tasks.guard.ProjectFileWatcher
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
@@ -12,13 +14,23 @@ open class GuardTask : DefaultTask(){
     private val channel = Channel<String>(UNLIMITED)
 
     @TaskAction
-    fun run() {
+    suspend fun run() {
         startAllProjectWatch()
         startConsoleWatch()
     }
 
-    private fun startAllProjectWatch() {
-        ProjectFileWatcher(project, channel).run()
+    private suspend fun startAllProjectWatch() {
+        coroutineScope {
+            launch {
+                ProjectFileWatcher(project, channel).run()
+            }
+            project.subprojects.forEach{
+                launch {
+                    ProjectFileWatcher(it, channel).run()
+                }
+            }
+        }
+
         project.subprojects.forEach{
             ProjectFileWatcher(it, channel).run()
         }
