1d3c5104492320eb8a35e8e19345762f84403626
jimxl <tianxiaxl@gmail.com>
2021-10-06 16:31:26 +0800

    修正了 com.adarshr.test-logger 插件没有被使用到各个子项目的问题

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "info.dreamcoder"
-version = "1.3.1"
+version = "1.3.2"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/gradle/plugins/DevTools.kt
+++ src/main/kotlin/gradle/plugins/DevTools.kt
@@ -1,11 +1,11 @@
 package gradle.plugins
 
 import com.adarshr.gradle.testlogger.TestLoggerExtension
+import com.adarshr.gradle.testlogger.theme.ThemeType
 import gradle.tasks.GuardTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.logging.LogLevel
-import java.net.URI
 
 
 class DevTools: Plugin<Project> {
@@ -13,19 +13,15 @@ class DevTools: Plugin<Project> {
     override fun apply(project: Project) {
         setupTestLoggerPlugin(project)
         project.tasks.create("guard", GuardTask::class.java) {
-            it.group = "dreamcoder"
+            it.group = "dreamcoder devtools"
         }
     }
 
     private fun setupTestLoggerPlugin(project: Project) {
-        project.repositories.maven {
-            it.url = URI("https://plugins.gradle.org/m2/")
-        }
-
         project.allprojects.forEach {
             it.pluginManager.apply("com.adarshr.test-logger")
-            project.extensions.configure(TestLoggerExtension::class.java) { testLoggerExtension ->
-                testLoggerExtension.theme = com.adarshr.gradle.testlogger.theme.ThemeType.MOCHA
+            it.extensions.configure(TestLoggerExtension::class.java) { testLoggerExtension ->
+                testLoggerExtension.theme = ThemeType.MOCHA
                 testLoggerExtension.showStackTraces = false
                 testLoggerExtension.showSummary = true
                 testLoggerExtension.logLevel = LogLevel.QUIET
